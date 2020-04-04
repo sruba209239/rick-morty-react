ffbabd055c28fbb24324158d5e3b3a829ec9e4b8
Sruba Mohapatra <sruba209239@gmail.com>
2020-04-04 23:04:26 +0530

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 class App extends React.Component {
     render() {
         return (
-            <div>Hello</div>
+            <div className="row">
+                <div className="col-md-4">part1</div>
+                <div className="col-md-6">part2</div>
+            </div>
         );
     };
 }
